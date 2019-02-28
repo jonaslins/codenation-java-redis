4549711f9e40495bbfbad9e0b1ae9f758cf6598e
Jonas <jonaslinsj@gmail.com>
2019-02-28 00:24:37 -0300

    Controller requests

--- src/main/java/challenge/RestaurantController.java
+++ src/main/java/challenge/RestaurantController.java
@@ -1,14 +1,21 @@
 package challenge;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.geo.Point;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/restaurants")
 public class RestaurantController {
 
 	@Autowired
 	private RestaurantService service;
 
-	public NeighborhoodRedis findInNeighborhood() {
-		return service.findInNeighborhood(0, 0);
+	@GetMapping("/findInNeighborhood")
+	public NeighborhoodRedis findInNeighborhood(Double x, Double y) {
+		return service.findInNeighborhood(x, y);
 	}
 
 }
