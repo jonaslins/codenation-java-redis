b13ba8ef14474bee6face12a168fa342232595d7
Jonas <jonaslinsj@gmail.com>
2019-02-28 00:38:50 -0300

    Create mongo data models

--- src/main/java/challenge/NeighborhoodMongo.java
+++ src/main/java/challenge/NeighborhoodMongo.java
@@ -1,10 +1,47 @@
 package challenge;
 
+import org.springframework.data.mongodb.core.geo.GeoJsonMultiPoint;
+import org.springframework.data.mongodb.core.geo.GeoJsonMultiPolygon;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.data.mongodb.core.geo.GeoJsonPolygon;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexType;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 /**
  * Classe para mapear o bairro no MongoDB
  *
  */
+@Document(collection = "neighborhood")
 public class NeighborhoodMongo {
 
+    @GeoSpatialIndexed(type = GeoSpatialIndexType.GEO_2DSPHERE)
+    private GeoJsonPolygon geometry;
+    @Field
+    private String name;
 
+    public NeighborhoodMongo() {
+    }
+
+    public NeighborhoodMongo(GeoJsonPolygon geometry, String name) {
+        this.geometry = geometry;
+        this.name = name;
+    }
+
+    public GeoJsonPolygon getGeometry() {
+        return geometry;
+    }
+
+    public void setGeometry(GeoJsonPolygon geometry) {
+        this.geometry = geometry;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/challenge/RestaurantMongo.java
+++ src/main/java/challenge/RestaurantMongo.java
@@ -1,10 +1,44 @@
 package challenge;
 
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexType;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 /**
  * Classe para mapear o restaurante no MongoDB
  *
  */
+@Document(collection = "restaurant")
 public class RestaurantMongo {
 
+    @GeoSpatialIndexed(type = GeoSpatialIndexType.GEO_2DSPHERE)
+    private GeoJsonPoint location;
+    @Field
+    private String name;
 
+    public RestaurantMongo() {
+    }
+
+    public RestaurantMongo(GeoJsonPoint location, String name) {
+        this.location = location;
+        this.name = name;
+    }
+
+    public GeoJsonPoint getLocation() {
+        return location;
+    }
+
+    public void setLocation(GeoJsonPoint location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
